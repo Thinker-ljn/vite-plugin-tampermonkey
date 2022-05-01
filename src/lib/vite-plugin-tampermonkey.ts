c81075023343526745f6dcbb6a2cfae9960ad16b
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2022-05-01 14:08:24 +0800

    fix: capable with css post-processing, e.g. UnoCSS

--- src/lib/vite-plugin-tampermonkey.ts
+++ src/lib/vite-plugin-tampermonkey.ts
@@ -31,72 +31,66 @@ function getAddress(
 
 export const DEV_TAMPERMONKEY_PATH = '/@tampermonkey-dev.user.js'
 const cyanColor = '\x1B[36m%s\x1B[0m'
-export default function tampermonkeyPlugin(options: Options = {}): Plugin {
-  const injectCss =
-    process.env.NODE_ENV === 'production'
-      ? {
-          transform: injectCssPluginOption.transform,
-          writeBundle: injectCssPluginOption.writeBundle,
-        }
-      : {}
-
+export default function tampermonkeyPlugin(options: Options = {}): Plugin[] {
   const autoGrantModuleParsed =
     options.autoGrant === false ? undefined : parserGrant.moduleParsed
-  return {
-    name: 'tampermonkey-dev-entry',
-    configureServer(server) {
-      return () => {
-        server.httpServer?.on('listening', () => {
-          const address = getAddress(server.httpServer?.address())
-
-          if (address) {
-            setTimeout(() => {
-              console.log(
-                cyanColor,
-                `> [Tampermonkey] - click link to install: http://${address.address}:${address.port}${DEV_TAMPERMONKEY_PATH}`
-              )
-            })
-          }
-        })
-        server.middlewares.use((req, res, next) => {
-          if (req.url === DEV_TAMPERMONKEY_PATH) {
+  return [
+    {
+      name: 'tampermonkey-dev-entry',
+      configureServer(server) {
+        return () => {
+          server.httpServer?.on('listening', () => {
             const address = getAddress(server.httpServer?.address())
-            if (address) {
-              res.write(genDevTampermonkeyCode(address))
-            } else {
-              res.write('应该不会出现这种情况...')
-            }
-          }
-          next()
-        })
-      }
-    },
-    moduleParsed: autoGrantModuleParsed,
-    ...injectCss,
-    config(config) {
-      // development
-      let hmr = config.server?.hmr
-      if (typeof hmr === 'boolean' || !hmr) {
-        hmr = {}
-      }
-      // 强制 hrm 指向 ws://127.0.0.1
-      hmr.protocol = 'ws'
-      hmr.host = '127.0.0.1'
-      config.server = {
-        ...(config.server || {}),
-        hmr,
-      }
 
-      // production
-      const rollupOptions = genRollupOptionsGenerator(bannerGen)(
-        options.externalGlobals
-      )
-      config.build = {
-        lib: gendefaultBuildOptions(),
-        rollupOptions,
-        minify: false,
-        cssCodeSplit: false,
-      }
+            if (address) {
+              setTimeout(() => {
+                console.log(
+                  cyanColor,
+                  `> [Tampermonkey] - click link to install: http://${address.address}:${address.port}${DEV_TAMPERMONKEY_PATH}`
+                )
+              })
+            }
+          })
+          server.middlewares.use((req, res, next) => {
+            if (req.url === DEV_TAMPERMONKEY_PATH) {
+              const address = getAddress(server.httpServer?.address())
+              if (address) {
+                res.write(genDevTampermonkeyCode(address))
+              } else {
+                res.write('应该不会出现这种情况...')
+              }
+            }
+            next()
+          })
+        }
+      },
+      moduleParsed: autoGrantModuleParsed,
+      config(config) {
+        // development
+        let hmr = config.server?.hmr
+        if (typeof hmr === 'boolean' || !hmr) {
+          hmr = {}
+        }
+        // 强制 hrm 指向 ws://127.0.0.1
+        hmr.protocol = 'ws'
+        hmr.host = '127.0.0.1'
+        config.server = {
+          ...(config.server || {}),
+          hmr,
+        }
+
+        // production
+        const rollupOptions = genRollupOptionsGenerator(bannerGen)(
+          options.externalGlobals
+        )
+        config.build = {
+          lib: gendefaultBuildOptions(),
+          rollupOptions,
+          minify: false,
+          cssCodeSplit: false,
+        }
+      },
     },
-  }
+    injectCssPluginOption,
+  ]
 }
